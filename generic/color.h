97e8afca3a90beb4bb7766ae5a2455ab99e971ea
Mac Mleczko <mlemacio0@gmail.com>
2023-03-03 11:14:44 -0600

    Reduce size of color.h

--- generic/color.h
+++ generic/color.h
@@ -1,52 +1,36 @@
 #ifndef GENERIC_COLOR_H
 #define GENERIC_COLOR_H
 
+#include <array>
 #include <cstdint>
 #include <ostream>
+
+enum class color_e : uint8_t
+{
+    RED = 0,
+    GREEN = 1,
+    BLUE = 2
+};
+
+static std::array<std::string, 3> color_to_string{
+    "RED",
+    "GREEN",
+    "BLUE",
+};
 
 class Color
 {
 public:
-    enum class color_e : uint8_t
-    {
-        RED = 0,
-        GREEN = 1,
-        BLUE = 2
-    };
-
     Color(color_e _c) : c(_c) {}
 
-    friend bool operator<(const Color &l, const Color &r)
+    friend auto operator<=>(const Color &l, const Color &r)
     {
-        return static_cast<uint8_t>(l.c) < static_cast<uint8_t>(r.c);
-    }
-
-    friend bool operator==(const Color &l, const Color &r)
-    {
-        return static_cast<uint8_t>(l.c) == static_cast<uint8_t>(r.c);
+        return static_cast<uint8_t>(l.c) <=> static_cast<uint8_t>(r.c);
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Color &obj)
     {
-        switch (obj.c)
-        {
-        case color_e::RED:
-        {
-            os << "RED";
-            break;
-        }
-        case color_e::GREEN:
-        {
-            os << "GREEN";
-            break;
-        }
-        case color_e::BLUE:
-        {
-            os << "BLUE";
-            break;
-        }
-        }
-
+        os << color_to_string[static_cast<uint8_t>(obj.c)];
         return os;
     }
 
